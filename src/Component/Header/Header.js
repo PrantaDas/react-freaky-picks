77e91c4d290c007992ea042591de0ac860a6a58d
PrantaDas <prantodas043@gmail.com>
2022-03-26 00:32:01 +0600

    first commit

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='nav'>
+            <h1 className='title'>Freaky Pick!</h1>
+            <h1>About Us</h1>
+        </div>
+    );
+};
+
+export default Header;
